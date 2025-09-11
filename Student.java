6ee1e0fd39867a3cdba66f0ce0de449160fc3349
MidKnight009 <91510256+MidKnight009@users.noreply.github.com>
2025-09-11 18:26:20 -0400

    Updated Student.java
    Check this out to see if you can work it in

--- Student.java
+++ Student.java
@@ -32,5 +32,24 @@ public class Student{
         return GPA;
 
     }
+    //Mutator for SSN
+    //Includes its own requesting code for SSN, maybe keep or continue to use the other one
+    public String changeSSN() {
+        Scanner scanner = new scanner(System.in);
+        System.out.println("Enter SSN: ");
+        String input = scanner.nextLine();
+
+        if (input.length() > 4) {
+            String last4 = input.substring(input.length() - 4);
+            String masked = "*".repeat(input.length() -4) + last4;
+            try {
+                SSN = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid")
+            }
+            return masked;
+        } else {
+            System.out.println("Invalid SSN, must be longer than4 digits");
+            return null;
 
 }
