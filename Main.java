7da38d9d0ce66138ecd692b87856923df25527ef
Dario Perez <145610289+DarioPerez27@users.noreply.github.com>
2025-09-11 12:19:20 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main{
+
+    public static void main(String[] args){
+
+        Student student1 = new Student();
+
+        student1.changeName();
+        System.out.println(student1.getname());
+        System.out.println(student1.getGPA());
+        System.out.println(student1.getSSN());
+
+    }
+}
---
+++ Student.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Student{
+
+    private String Name = "Dicky";
+    private int SSN = 123456789;
+    private double GPA = 4.0;
+
+    public String getname() {
+
+        return Name;
+
+    }
+
+    public String changeName(){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a name: ");
+        Name = scanner.nextLine();
+        return Name;
+
+    }
+
+    public int getSSN(){
+
+        return SSN;
+
+    }
+
+    public double getGPA(){
+
+        return GPA;
+
+    }
+
+}
